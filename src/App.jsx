1b7f29e82d3e45d3c06807ec2fac29c0e43a15e0
nisalrenuja <nrenuja@gmail.com>
2023-08-27 16:35:06 +0530

    asserts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-green-700">
-      Hello world!
-    </h1>
-  );
-}
+const App = () => (
+  <main className="relative">
+    Nav<section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super Quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offers</section>
+    <section className="padding bg-pale-blue">Customer Reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="padding-x pb-8 padding-t bg-black">Footer</section>
+  </main>
+);
+
+export default App;
