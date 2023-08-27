2c46a1360447da9d7772ae00b0db646ac58a2a21
nisalrenuja <nrenuja@gmail.com>
2023-08-27 18:05:07 +0530

    nav bar added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+import { hamburger } from "../assets/icons";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="max-lg:block hidden">
+          <img src={hamburger} width={25} height={25} alt="hamburger" />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
