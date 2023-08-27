dd63e58da2982a7350cd564e4e882758e18291c0
nisalrenuja <nrenuja@gmail.com>
2023-08-27 17:09:51 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import {
+  CustomerReviews,
+  CustomerOffers,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav<section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Quality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">Special Offers</section>
-    <section className="padding bg-pale-blue">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="padding-x pb-8 padding-t bg-black">Footer</section>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <CustomerOffers />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x pb-8 padding-t bg-black">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerOffers from "./CustomerOffers";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerOffers,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  Subscribe,
+  SuperQuality,
+};
