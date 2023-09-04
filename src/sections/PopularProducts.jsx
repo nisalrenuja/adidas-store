3c904f9b8e5389ea1bce35fe194bf8155f039956
nisalrenuja <nrenuja@gmail.com>
2023-09-04 06:41:14 +0530

    popular products added

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { products } from "../constants";
+import { PopularProductCard } from "../components";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="bg-black text-white px-2">Popular</span> Products
+        </h2>
+        <p className="text-slate-gray font-montserrat text-lg lg:max-w-lg mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+          pellentesque amet ullamcorper gravida.
+        </p>
+      </div>
+      <div className="mt-16 grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:"
+    >
+      SuperQuality
+    </section>
+  );
 };
 
 export default SuperQuality;
