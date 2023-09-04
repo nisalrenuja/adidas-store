68d4698e9d8cef217579638f01b4df70ecbe7420
nisalrenuja <nrenuja@gmail.com>
2023-09-04 06:40:19 +0530

    popular product card added

---
+++ src/components/PopularProductCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { star } from "../assets/icons";
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="flex justify-start gap-2.5 mt-8">
+        <img src={star} height={24} width={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+          (4.5)
+        </p>
+      </div>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+        {name}
+      </h3>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-montserrat text-coral-red">
+        {price}
+      </h3>
+    </div>
+  );
+};
+
+export default PopularProductCard;
