2d4a2ab92fa2b108f74138b7d0d858374a2df457
nisalrenuja <nrenuja@gmail.com>
2023-09-10 23:25:29 +0530

    services section fixes

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { services } from "../constants";
+import { ServiceCard } from "../components";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((services) => (
+        <ServiceCard key={services.label} {...services} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
