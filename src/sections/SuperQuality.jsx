1793804096ca99c3f3dee0ea7bf24476c73206b6
nisalrenuja <nrenuja@gmail.com>
2023-09-10 23:25:43 +0530

    super quality section added

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import { bigShoe2 } from "../assets/images";
+import { Button } from "../components";
 
 const SuperQuality = () => {
   return (
     <section
       id="about-us"
-      className="flex justify-between items-center max-lg:"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
     >
-      SuperQuality
+      <div className="flex flex-col">
+        <h2 className="bg-black text-white p-2 font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          We Provide You
+          <span className="text-white "> Super </span>
+          <span className="text-white ">Quality </span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={bigShoe2}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
     </section>
   );
 };
